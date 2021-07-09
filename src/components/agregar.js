1f80cce22199718006b931af2f1f6ae5b520db05
Hector10101 <hfsa2327@gmail.com>
2021-07-09 00:21:15 -0400

    agregar agregando

--- src/components/agregar.js
+++ src/components/agregar.js
@@ -1,5 +1,14 @@
 import Route from "../libs/route";
 
+//Variables DOM
+let inputMonto,
+   selectCategoria,
+   radioMoneda,
+   inputFecha,
+   textareaDescripcion,
+   contentAlertAgregar;
+
+
 class Agregar extends Route {
    constructor() {
       super("agregar", {
@@ -11,42 +20,73 @@ class Agregar extends Route {
 
    async whenMounted() {
       //When document ready
-      this.agregar();
+      document.getElementById("formAgregar").addEventListener('submit', (e)=> {
+         e.preventDefault(); 
+         this.agregar();
+      });
+
+      //Asignar el elemento a las Variables DOM
+      this.getElements();
    }
    
    agregar() {
-
-      document.getElementById("formAgregar").addEventListener('submit', (e)=>{
-         e.preventDefault();
-         const inputMonto = document.getElementById("inputMonto");
-         // const selectMoneda = document.getElementById("selectMoneda");
-         const selectCategoria = document.getElementById("selectCategoria");
-         const inputFecha = document.getElementById("inputFecha");
-         const textareaDescripcion = document.getElementById("textareaDescripcion");
+      this.getElements();
+      let gastos = [];
+      let nuevoGasto = {
+         monto: inputMonto.value,
+         moneda: radioMoneda.value,
+         categoria: selectCategoria.value,
+         fecha: inputFecha.value,
+         descripcion: textareaDescripcion.value
+      }
       
-         let gastos = [];
-         let nuevoGasto = {
-            monto: inputMonto.value,
-            // moneda: selectMoneda.value,
-            categoria: selectCategoria.value,
-            fecha: inputFecha.value,
-            descripcion: textareaDescripcion.value
-         }
+      if(localStorage.getItem("Gastos")){
+         gastos = JSON.parse(localStorage.getItem("Gastos"));
+         this.addValueLocalStorage(nuevoGasto, gastos);
          
-         if(localStorage.getItem("Gastos")){
-            gastos = JSON.parse(localStorage.getItem("Gastos"));
-            gastos.unshift(nuevoGasto);
-            console.log(gastos);
-            localStorage.setItem("Gastos", JSON.stringify(gastos));
-            
-         }else{
-            gastos.unshift(nuevoGasto);
-            console.log(gastos);
-            localStorage.setItem("Gastos", JSON.stringify(gastos));
-         }
-      });
+      }else{
+         this.addValueLocalStorage(nuevoGasto, gastos);
+      }
+
+      this.alertAgregar();
    }
 
+   getElements() {
+      selectCategoria = document.getElementById("selectCategoria");
+      inputMonto = document.getElementById("inputMonto");
+      radioMoneda = document.querySelector("input[name='Moneda']:checked");
+      inputFecha = document.getElementById("inputFecha");
+      textareaDescripcion = document.getElementById("textareaDescripcion");
+
+      contentAlertAgregar = document.getElementById("contentAlertAgregar");
+   }
+
+   cleanValueElements() {
+      inputMonto.value = "";
+      inputFecha.value = "";
+      textareaDescripcion.value = "";
+   }
+
+   addValueLocalStorage(newGasto, Gastos) {
+      Gastos.unshift(newGasto);
+      localStorage.setItem("Gastos", JSON.stringify(Gastos));
+      this.cleanValueElements();
+   }
+
+   alertAgregar() {
+      //Mostar Alerta de agregado
+      contentAlertAgregar.innerHTML = `
+      <div class="alert alert-success alertAgregar mt-3">
+         <img src="./assets/img/check.svg" alt="" class="me-2"  width="28" height="28">
+         <strong>Agregado</strong>
+      </div>
+      `;
+
+      //Aliminar Alerta de agregado
+      setTimeout(() => {
+         contentAlertAgregar.innerHTML = "";
+      }, 3500);
+   }
 }
 
 const agregar = new Agregar();
