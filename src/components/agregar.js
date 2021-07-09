34bf5c9c2f7ff9890ccb9922a54d204cff6ffb0b
Hector10101 <hfsa2327@gmail.com>
2021-07-08 20:38:43 -0400

    funcion agregar

--- src/components/agregar.js
+++ src/components/agregar.js
@@ -10,6 +10,37 @@ class Agregar extends Route {
    
    agregar() {
       console.log("Hola agregar");
+      document.getElementById("formAgregar").addEventListener('submit', (e)=>{
+         e.preventDefault();
+         const inputMonto = document.getElementById("inputMonto");
+         const selectMoneda = document.getElementById("selectMoneda");
+         const selectCategoria = document.getElementById("selectCategoria");
+         const inputFecha = document.getElementById("inputFecha");
+         const textareaDescripcion = document.getElementById("textareaDescripcion");
+      
+         let gastos = [];
+         let nuevoGasto = {
+            monto: inputMonto.value,
+            moneda: selectMoneda.value,
+            categoria: selectCategoria.value,
+            fecha: inputFecha.value,
+            descripcion: textareaDescripcion.value
+         }
+         
+         if(localStorage.getItem("Gastos")){
+            gastos = JSON.parse(localStorage.getItem("Gastos"));
+            gastos.unshift(nuevoGasto);
+            console.log(gastos);
+            localStorage.setItem("Gastos", JSON.stringify(gastos));
+            
+         }else{
+            gastos.unshift(nuevoGasto);
+            console.log(gastos);
+            localStorage.setItem("Gastos", JSON.stringify(gastos));
+         }
+      
+         
+      });
    }
 }
 
