074eee9e0b9f4e5998590443000f38974841da26
Miguel Mateo <miguelmateot94@gmail.com>
2021-07-13 18:31:25 -0400

    Cambios en categoria, guarda en localstorage

--- src/components/categorías.js
+++ src/components/categorías.js
@@ -1,11 +1,78 @@
 import Route from "../libs/route";
+import { changeFormStatusBootstrap, saveCategorias } from "./funciones"
+
+let nombreCategoria;
+let tipoCategoria;
+let contentAlertAgregar;
 
 class Categorias extends Route {
-   constructor(){
-      super("categorias",{
+   constructor() {
+      super("categorias", {
          htmlName: "/views/categorias.html",
          default: true
       });
+      this.onMountCb = this.whenMounted;
+   }
+
+   async whenMounted() {
+
+      //When document ready
+
+      document.getElementById("formCategoria").addEventListener('submit', (e) => {
+         e.preventDefault();
+         this.agregarCategoria();
+      });
+
+      //Asignar el elemento a las Variables DOM
+      this.getElements();
+   }
+
+   agregarCategoria() {
+      console.log(this.getElements());
+      let nuevaCategoria = {
+         nombre: nombreCategoria.value,
+         tipo: tipoCategoria.value,
+      }
+
+      if (nombreCategoria.value !== '' &&
+         tipoCategoria.value !== '') {
+
+         saveCategorias(nuevaCategoria);
+
+         this.cleanValueElements();
+
+         this.alertAgregar();
+
+         changeFormStatusBootstrap(true);
+      } else {
+         changeFormStatusBootstrap(false);
+      }
+   }
+
+   getElements() {
+      nombreCategoria = document.getElementById('nombreCategoria');
+      tipoCategoria = document.getElementById('tipoCategoria');
+      contentAlertAgregar = document.getElementById("contentAlertAgregar");
+   }
+
+   cleanValueElements() {
+      nombreCategoria.value = "";
+      tipoCategoria.value = "";
+   }
+
+   alertAgregar() {
+      //Mostar Alerta de agregado
+      contentAlertAgregar.innerHTML = `
+      <div class="alert alert-success alertAgregar mt-3">
+         <img src="./assets/img/check.svg" alt="" class="me-2"  width="28" height="28">
+         <strong>Agregado</strong>
+      </div>
+      `;
+
+      //Aliminar Alerta de agregado
+      setTimeout(() => {
+         contentAlertAgregar.innerHTML = "";
+      }, 3500);
    }
 }
 
--- src/components/funciones.js
+++ src/components/funciones.js
@@ -1,7 +1,5 @@
 //Aquí pueden ir funciones que todos pueden usar
 
-
-
 function changeFormStatusBootstrap(validado) {
    //Solo cambia la clase de cuando necesita ser validado o no.
    //Debe de recibir un true o un false
@@ -38,6 +36,28 @@ function updateGastos(gastos) {
    localStorage.setItem("Gastos", JSON.stringify(gastos));
 }
 
+function saveCategorias(nuevaCategoria)
+{
+   let categorias = getCategorias();
+   categorias.push(nuevaCategoria);
+   updateCategoria(categorias);
+}
+
+function getCategorias() {
+   let categorias = JSON.parse(localStorage.getItem("Categorias"));
+
+   if (categorias == null) {
+      categorias = [];
+      updateGastos(categorias);
+   }
+   return categorias;
+}
+
+function updateCategoria(categorias)
+{
+   localStorage.setItem("Categorias", JSON.stringify(categorias));
+}
+
 //Fin funciones relacionadas con los Gastos
 
-export { changeFormStatusBootstrap, saveGastos, getGastos }
+export { changeFormStatusBootstrap, saveGastos, getGastos, saveCategorias, getCategorias }
